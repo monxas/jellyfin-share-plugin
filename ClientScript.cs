01ed326cc6cfc96007a5a0c5ec15a7ba33c33dca
Ramon Kamibayashi <ramonkawa@gmail.com>
2026-01-02 15:15:29 +0100

    Add no-cache headers to client.js to prevent CDN caching

--- ClientScript.cs
+++ ClientScript.cs
@@ -30,6 +30,15 @@ public class ClientScriptController : ControllerBase
         using var reader = new StreamReader(stream);
         var script = reader.ReadToEnd();
 
+        // Prevent caching - especially important for Cloudflare
+        Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+        Response.Headers["Pragma"] = "no-cache";
+        Response.Headers["Expires"] = "0";
+
+        // Add version as ETag for cache busting
+        var version = assembly.GetName().Version?.ToString() ?? "1.0.0";
+        Response.Headers["ETag"] = $"\"{version}\"";
+
         return Content(script, "application/javascript");
     }
 }
